9fe3baa6b4768e0ac266991de413cfe444b7888b
Qasim Khawaja <q.khawaja@yahoo.com>
2022-03-20 22:55:15 -0600

    feat: add status checks

--- play.py
+++ play.py
@@ -84,7 +84,7 @@ def playSingleGame(alternative=False):
     print(winner, istimeout)
     return result,istimeout
 
-def playGames(numGame=10):
+def playGames(numGame=1):
     global win1,win2,numTimeout
     for i in range(0,numGame):
         if(i<numGame/2):
