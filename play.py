5566dd7dffd8f952d4eb3f7e66eb9e112138a58c
Qasim Khawaja <q.khawaja@yahoo.com>
2022-04-03 17:47:24 -0600

    fix: prepare for final submission

--- play.py
+++ play.py
@@ -4,7 +4,7 @@ import pexpect
 # path to the two players
 # player 1 plays first
 player1='random_player/nogo_random.py'
-player2='nogo4/nogo4.py' 
+player2='go_getters/nogo4.py' 
 
 # stats for win/timeout
 # note that timeout is considered lost
